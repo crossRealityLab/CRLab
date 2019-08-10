2962a9670521c7a7262edd7fde2d0df188dc511b
Ricky Hsieh <ric1134991@gmail.com>
2019-08-10 19:48:34 +0800

    Updates

---
+++ precache-manifest.7e0fa161b45ffd59a0b8907e031e4c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e999c33454dc584843a373819c8226b8",
+    "url": "/CRLab/index.html"
+  },
+  {
+    "revision": "404b670b5074d2e1ac49",
+    "url": "/CRLab/static/js/2.14170bf0.chunk.js"
+  },
+  {
+    "revision": "142ceeca7a217e0ae5d0",
+    "url": "/CRLab/static/js/main.c13d7b68.chunk.js"
+  },
+  {
+    "revision": "d5d8cce5ca405d803c7f",
+    "url": "/CRLab/static/js/runtime~main.0cd0de30.js"
+  },
+  {
+    "revision": "b6681619dcc616623083589aa826d2be",
+    "url": "/CRLab/static/media/noFoundImgPerson.b6681619.png"
+  },
+  {
+    "revision": "030b788ca6c7c5b79c531f18f96163d8",
+    "url": "/CRLab/static/media/test.030b788c.png"
+  }
+]);
